9608c3f0e457d4d4bd699f1b2f6866df58223538
rawfid <rawfid.shuvo@outlook.com>
2025-12-31 06:30:33 +0100

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -2790,7 +2790,7 @@ export default function MasqueradeProtocol() {
                   `}
                 >
                   {/* Pulse dot if glitch ready */}
-                  {!me.glitchUsed && !me.isEliminated && (
+                  {!me.glitchUsed && !me.isEliminated && me.isMyTurn && (
                     <span className="absolute -top-1 -right-1 flex h-3 w-3">
                       <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
                       <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
